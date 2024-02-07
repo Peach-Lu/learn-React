7fa79469484c058e72d4ef25a2bb71cde5df31bb
Peach <2990221345@qq.com>
2024-02-08 03:02:46 +0800

    购物车案例

---
+++ 05_购物车阶段案例/data.js
@@ -0,0 +1,6 @@
+const books = [
+  { id: 1, name: '算法导论', date: '2006-9', price: 99.00, count: 1 },
+  { id: 2, name: 'linux编程艺术', date: '2006-9', price: 59.00, count: 1 },
+  { id: 3, name: '编程珠玑', date: '2006-9', price: 128.00, count: 1 },
+  { id: 4, name: '代码大全', date: '2006-9', price: 199.00, count: 1 }
+]
