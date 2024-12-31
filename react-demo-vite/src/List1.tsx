bc0fa715ea296a3adf77d13ce39c1f1b695fb2d9
Peach <2990221345@qq.com>
2024-12-31 20:05:03 +0800

    update 增加immer 状态更改

--- react-demo-vite/src/List1.tsx
+++ react-demo-vite/src/List1.tsx
@@ -1,4 +1,4 @@
-import React,{FC, useState,} from "react";
+import React,{FC, useState, useEffect} from "react";
 import {produce} from 'immer'
 import QuestionCard from './components/QuestionCard'
 const List1:FC = ()=>{
