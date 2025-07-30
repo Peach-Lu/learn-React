d415034c1ae3b95b4a4ab6ffc5d0affba67a7a4c
Peach <2990221345@qq.com>
2025-07-30 23:59:02 +0800

    updata 增加hook逻辑复用

--- wenjuan-fe/src/pages/question/Edit/index.tsx
+++ wenjuan-fe/src/pages/question/Edit/index.tsx
@@ -2,25 +2,27 @@ import React, { FC, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getQuestionService } from "@/request/question";
 import type { ResDataType } from "@/request/http";
+import useLoadingQuestionData from "@/hook/useLoadingQuestionData";
 const Edit: FC = () => {
-  const { id } = useParams();
-  const [ResData, setResData] = useState<ResDataType>({});
-  const [loading, setLoading] = useState(false);
-  console.log("id", id);
-  useEffect(() => {
-    async function fn() {
-      const ResData = await getQuestionService(id);
-      console.log("ResData", ResData);
-      setResData(ResData);
-      setLoading(false);
-    }
-    fn();
-  }, []);
+//   const { id } = useParams();
+//   const [ResData, setResData] = useState<ResDataType>({});
+//   const [loading, setLoading] = useState(false);
+//   console.log("id", id);
+//   useEffect(() => {
+//     async function fn() {
+//       const ResData = await getQuestionService(id);
+//       console.log("ResData", ResData);
+//       setResData(ResData);
+//       setLoading(false);
+//     }
+//     fn();
+//   }, []);
+const { loading, questionData } = useLoadingQuestionData();
 
   return (
     <>
       <p>Edit</p>
-      {loading ? <p>Loading...</p> : <p>{JSON.stringify(ResData)}</p>}
+      {loading ? <p>Loading...</p> : <p>{JSON.stringify(questionData)}</p>}
     </>
   );
 };
--- wenjuan-fe/src/pages/question/Stat/index.tsx
+++ wenjuan-fe/src/pages/question/Stat/index.tsx
@@ -1,7 +1,13 @@
 import React,{ FC} from "react";
+import useLoadingQuestionData from "@/hook/useLoadingQuestionData";
+
 const  Stat:FC = ()=>{
+    const { loading, questionData } = useLoadingQuestionData();
+    
   return <>
   <p>Stat</p>
+      {loading ? <p>Loading...</p> : <p>{JSON.stringify(questionData)}</p>}
+
   </>
 }
 export default Stat
